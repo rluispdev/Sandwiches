c60cc5a3043673f49924d11de791b577f7bd05a5
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2023-09-28 11:19:42 -0300

    Comit comentado 2

--- Sandwiches/ContentView.swift
+++ Sandwiches/ContentView.swift
@@ -5,37 +5,53 @@
 //  Created by Rafael Gonzaga on 28/09/23.
 //
 
-import SwiftUI
 
 import SwiftUI
 
 struct ContentView: View {
-    /// 1 - passando o array de dados  vazio criados aqui.
+    /// 1 - Passando o array de dados  vazio criados aqui.
     
     var sandwiches: [Sandwich] = []
     
     var body: some View {
-       /// 3 - passo a variavel que recebe o array vazio
-        List(sandwiches) { item in
+       /// 3 - Passo a variavel que recebe o array vazio
+       
+        ///6 - Adicioando um titulo
+        NavigationStack{
             
-            HStack {
-                Image(item.imageName)
-                    .resizable()
-                    .scaledToFill()
-                    .frame(width: 40, height: 40)
-                
-                VStack(alignment: .leading) {
-                    /// 4 - Aqui consigo acessar o titulo, chamndo item.name
-                    Text(item.name)
-                
-                    ///5 - Aqui consigo acessar o numero de ingredientes por meio de uma interpolação, devido o ingredientCont ser do tipo Int.
-                    Text("\(item.ingredientCount) ingredients")
-                        .font(.subheadline)
-                        .foregroundStyle(.secondary)
+            List(sandwiches) { item in
+             
+                HStack {
+                    // A - Colocando um Text aqui resolve... 😮‍💨
+                //   Text("")
+                      
+                    ///8 - Adicionando uma navigation Link passando item.name
+                    NavigationLink(destination: Text(item.name)) {
+                        Image(item.imageName)
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 40, height: 40)
+                            .cornerRadius(3.0)
+                        
+                        VStack(alignment: .leading) {
+                            
+                            /// 4 - Aqui consigo acessar o titulo, chamndo item.name
+                            Text(item.name)
+                            
+                            ///5 - Aqui consigo acessar o numero de ingredientes por meio de uma interpolação, devido o ingredientCont ser do tipo Int.
+                            Text("\(item.ingredientCount) ingredients")
+                                .font(.subheadline)
+                                .foregroundStyle(.secondary)
+                        }
+                    }
+      
                 }
-          
+                
             }
+            ///7 - Escrevendo o titulo.
+            .navigationTitle("Sandwiches")
         }
+       
     }
 }
 
