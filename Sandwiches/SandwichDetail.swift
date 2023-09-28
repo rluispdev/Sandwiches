71ad8d4e81b6feb3d23f2a1523194a607c426da1
Rafael Gonzaga via Xcode <rluisp@icloud.com>
2023-09-28 19:56:52 -0300

    Adicionando Zoom

---
+++ Sandwiches/SandwichDetail.swift
@@ -0,0 +1,18 @@
+//
+//  SandwichDetail.swift
+//  Sandwiches
+//
+//  Created by Rafael Gonzaga on 28/09/23.
+//
+
+import SwiftUI
+
+struct SandwichDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SandwichDetail()
+}
---
+++ Sandwiches/SandwichViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SandwichViewController.swift
+//  Sandwiches
+//
+//  Created by Rafael Gonzaga on 28/09/23.
+//
+
+import SwiftUI
+
+struct SandwichViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SandwichViewController()
+}
